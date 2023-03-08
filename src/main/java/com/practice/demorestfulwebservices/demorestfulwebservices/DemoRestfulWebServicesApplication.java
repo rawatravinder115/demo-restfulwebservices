16e2da47db6a07301910e0cd85aa2ad00ff65d74
ravinderRawat <rawatravinder115@gmail.com>
2023-03-08 15:54:12 +0530

    first commit

---
+++ src/main/java/com/practice/demorestfulwebservices/demorestfulwebservices/DemoRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.practice.demorestfulwebservices.demorestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/demorestfulwebservices/demorestfulwebservices/DemoRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.demorestfulwebservices.demorestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
