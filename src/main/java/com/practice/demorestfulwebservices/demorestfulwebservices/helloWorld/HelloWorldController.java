d80fd64aaf000217ce028b357a14f245ff5451aa
ravinderRawat <rawatravinder115@gmail.com>
2023-03-08 16:34:55 +0530

    add comparison b/w json and string

---
+++ src/main/java/com/practice/demorestfulwebservices/demorestfulwebservices/helloWorld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.practice.demorestfulwebservices.demorestfulwebservices.helloWorld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello-World-fuck";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello-World-fuck");
+    }
+
+}
